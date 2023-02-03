5c0e265962bc819bc42c0d627a16ab8af158f2c0
Myrza Alymbekov <myrza-alymbekov@MacBook-Air-Myrza.local>
2023-02-03 10:21:46 +0600

    Second Commit

--- bag_fix/forms.py
+++ bag_fix/forms.py
@@ -1,14 +1,25 @@
+from django import forms
 from django.forms import ModelForm, FileField, ClearableFileInput
 
 from .models import Complaints
 
 
 class ComplaintsForm(ModelForm):
-    files = FileField(widget=ClearableFileInput(attrs={'multiple': True}), required=False)
+
+    files = FileField(label="", widget=ClearableFileInput(attrs={'multiple': True}), required=False)
 
     class Meta:
         model = Complaints
-        fields = '__all__'
+        fields = ['name', 'text', 'importance']
         read_only_fields = ['date']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Необязательное поле"}),
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 2, 'style': "margin-bottom: 30px;"}),
+            # 'importance': forms.CheckboxInput(attrs={'class': 'custom-control-input'}),
+        }
 
-
+        labels = {
+            'name': 'Вашe имя:',
+            'text': 'Описание:',
+            'importance': 'Срочность',
+        }
